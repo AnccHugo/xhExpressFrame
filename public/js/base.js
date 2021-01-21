a38fd46c5328323ad1b3f84888c38abb30366003
Ancc Hugo <sgr_hl_cc@aliyun.com>
2021-01-21 18:03:07 +0800

    暂存

--- public/js/base.js
+++ public/js/base.js
@@ -2,10 +2,6 @@ $(function () {
     // 网站标题
     window.document.title = config.siteTitle;
 
-    // 网站顶部导航
-    // if (config && config.menu) {
-    //     config.menu.forEach(function (val, idx, arr) {
-    //         $('#pageMenu').appendTo();
-    //     });
-    // }
+
+
 });
