1405c663592950ba8863997667b5942367a93996
Ancc Hugo <sgr_hl_cc@aliyun.com>
2021-01-21 10:41:15 +0800

    header部分完成

--- public/js/base.js
+++ public/js/base.js
@@ -1,4 +1,11 @@
 $(function () {
+    // 网站标题
     window.document.title = config.siteTitle;
 
+    // 网站顶部导航
+    // if (config && config.menu) {
+    //     config.menu.forEach(function (val, idx, arr) {
+    //         $('#pageMenu').appendTo();
+    //     });
+    // }
 });
