22f608ff131a4de120b45080f4aaefd8407fd8ce
Ancc Hugo <sgr_hl_cc@aliyun.com>
2021-01-26 17:26:41 +0800

    首页 - 特色推荐

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 $(function () {
     var lunbotu = new xhLunbotu($('#bannerView li'), $('#bannerControl li'), $('#bannerPrev'), $('#bannerNext'));
-    
 
+    _Init();
 });
+
+
+
+function _Init() {
+    _Bind_Tesetuijian();
+}
+function _Bind_Tesetuijian() {
+    $('#productTesetuijian .product_menu li').each(function (idx, ele) {
+        $(ele).mouseenter(function () {
+            $(ele).addClass('active').siblings().removeClass('active');
+            $($('#productTesetuijian .product_content li')[idx]).addClass('active').siblings().removeClass('active');
+        });
+    });
+}
