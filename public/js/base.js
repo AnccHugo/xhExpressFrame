64d79ab60d6502da69cf21f33891316a2219379a
Ancc Hugo <sgr_hl_cc@aliyun.com>
2021-01-30 10:31:17 +0800

    用户盒子 - 样式切换

--- public/js/base.js
+++ public/js/base.js
@@ -11,10 +11,40 @@ $(function () {
 
 function Init(){
     Bind_MaskUser();
+    Bind_Userbox();
 }
 
 function Bind_MaskUser() {
     // 打开用户界面遮罩
-    // $("#")
+    $("#aLogin").on('click', function () {
+        $('#maskUser').show();
+        $('.to_login').trigger('click');
+    });
+
+    $("#aRegiste").on('click', function () {
+        $('#maskUser').show();
+        $('.to_registe').trigger('click');
+    });
+
+    // 关闭用户界面遮罩
+    $('.click_off').on('click', function () {
+        $('#maskUser').hide();
+    })
+}   
+
+function Bind_Userbox() {
+    $('.user_tab li').each(function (idx, ele) {
+        $(ele).on('click', function () {
+            $(this).addClass('active').siblings().removeClass('active');
+
+            if (idx == 0) {
+                $('#divRegiste').hide();
+                $('#divLogin').show();
+            } else if (idx == 1) {
+                $('#divLogin').hide();
+                $('#divRegiste').show();
+            }
+        });
+    });
 }
 
