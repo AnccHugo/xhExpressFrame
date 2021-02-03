0e09fe2e9d4c93e67612dd2347101a8a488ff851
Ancc Hugo <sgr_hl_cc@aliyun.com>
2021-02-03 09:55:05 +0800

    no message

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 
 module.exports = {
-    domain: "localhost",
-    webPort: 80,
+    domain: "0.0.0.0",
+    webPort: 8080,
 
     isRelease: false,
     isOnLinux: false,
@@ -13,14 +13,14 @@ module.exports = {
     sslKeyPath: path.join(__dirname, '../private/www.ctcc.online.key'),
     sslPemPath: path.join(__dirname, '../private/www.ctcc.online.pem'),
 
-    dbConfig: {
-        "testDB": {
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            passwod: "ctcc123..",
-            database: "ctcc",
-        }
-    }
+    // dbConfig: {
+    //     "testDB": {
+    //         host: "localhost",
+    //         port: 3306,
+    //         user: "root",
+    //         passwod: "ctcc123..",
+    //         database: "ctcc",
+    //     }
+    // }
 
 };
